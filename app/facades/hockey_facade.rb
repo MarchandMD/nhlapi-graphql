7854908227d0e906bc96f3abfe1aa399f696d315
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-02-15 19:48:51 -0700

    Adds #specific_team to hockeyFacade

--- app/facades/hockey_facade.rb
+++ app/facades/hockey_facade.rb
@@ -1,8 +1,13 @@
 class HockeyFacade
   def all_teams
-    all_teams = service.all_teams
+    all_teams = service.all_teams[:teams]
 
     all_teams.map{ |team| Team.new(team) }
+  end
+
+  def specific_team(id)
+    team = service.specific_team(id)[:teams].first
+    Team.new(team)
   end
 
   def service
